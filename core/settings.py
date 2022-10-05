7f1d54b91185c90a21b9148f491b7d8d60e24102
Durashka69 <gulyashshsh@gmail.com>
2022-10-05 16:03:08 +0600

    Authentication bug fix

--- core/settings.py
+++ core/settings.py
@@ -149,10 +149,13 @@ SWAGGER_SETTINGS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 REST_USE_JWT = True
